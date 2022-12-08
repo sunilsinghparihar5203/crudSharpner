5596564be52c2a0aa88f34c7fd5d5bc01c17cb99
sanju5203 <>
2022-12-08 18:55:02 +0000

    try catch

--- script.js
+++ script.js
@@ -1,27 +1,14 @@
-console.log('person1 : shows ticket')
-console.log('person2 : shows ticket')
-
 const preMovie = async () => {
   const promiseWifeBringingtickets = new Promise((resolve, reject) => {
-    setTimeout(() => resolve('ticket'), 3000);
+    setTimeout(() => reject('ticket'), 3000);
   })
-  const getPopcorn = new Promise((resolve, reject) => resolve(`popcorn`));
-
-  const getCandy = new Promise((resolve, reject) => resolve('Candy'));
-
-  const getCoke = new Promise((resolve, reject) => resolve('Coke'))
-
-  let ticket = await promiseWifeBringingtickets;
-
-  let [popcord, candy, coke] = await Promise.all([getPopcorn, getCandy, getCoke])
-
-  console.log(`${popcord} ,${candy}, ${coke}`)
-  return ticket;
+  let ticket;
+  try {
+    ticket = await promiseWifeBringingtickets;
+  } catch (e) {
+    ticket = 'sad face';
+  }
+  return ticket
 };
 
-
 preMovie().then((m) => console.log(`person 3 : show ${m}`));
-
-
-console.log('person4 : shows ticket')
-console.log('person5 : shows ticket')
