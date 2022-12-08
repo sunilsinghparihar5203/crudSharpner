7c892c5a30a0dd996f78b9e8af68076626adbc0c
sunil singh parihar <78852176+sunilsinghparihar5203@users.noreply.github.com>
2022-12-08 16:59:47 +0000

    task 4

--- script.js
+++ script.js
@@ -3,6 +3,52 @@ const posts = [
   { CreatedAt: new Date().getTime(), Title: "Post Two", body: "this is post two body" }
 ];
 
+
+function getPosts() {
+  let output = ''
+  posts.forEach((post, index) => {
+    output += `<li>${post.Title} created ${Math.floor(msToTime(new Date().getTime() - post.CreatedAt))} seconds ago</li>`;
+  })
+  document.body.innerHTML = output
+  console.log(posts)
+}
+ getPosts()
+//setInterval(getPosts, 1000);
+
+
+function deletePost(posts) {
+  setTimeout(() => {
+    posts.pop()
+    console.log("deleted after 1 sec.")
+    getPosts()
+  }, 1000)
+};
+
+function createPost(post) {
+
+  return new Promise((resolve, reject) => {
+    posts.push(post)
+    const error = false
+    if (!error) {
+      
+      console.log("new post created")
+      getPosts()
+      resolve()
+    } else {
+      reject("Error: something went wrong!")
+    }
+  });
+
+}
+
+
+createPost({ CreatedAt: new Date().getTime(), Title: "Post four", body: "this is post three body." }, getPosts).then(() => {
+  deletePost(posts)
+}).then(getPosts).catch(err => console.log("some thing went wrong."))
+
+
+
+
 function msToTime(s) {
   var ms = s % 1000;
   s = (s - ms) / 1000;
@@ -23,58 +69,3 @@ function msToTime(s) {
   }
 
 }
-
-function getPosts() {
-  let output = ''
-    setTimeout(() => {
-         posts.forEach((post, index) => {
-        output += `<li>${post.Title} created ${Math.floor(msToTime(new Date().getTime() - post.CreatedAt))} seconds ago</li>`;
-      })
-     
-      document.body.innerHTML = output
-      console.log(posts)
-    }, 1000);
-}
-setInterval(getPosts, 1000);
-
-
-
-
-function deletePost(posts) {
-   return new Promise((resolve, reject) => {
-       posts.pop()
-      const error = false
-      if (!error) {
-        resolve()
-      } else {
-        reject("Error: something went wrong!")
-      }
-  });
-
-}
-
-function createPost(post) {
-
-  return new Promise((resolve, reject) => {
-      posts.push(post)
-    if(post.length >0){
-      const error = false
-    }else{
-      const error = true
-    }
-      
-      if (!error) {
-        resolve()
-      } else {
-        reject("Error: something went wrong!")
-      }
-  });
-
-}
-
-
-createPost({ CreatedAt: new Date().getTime(), Title: "Post Three", body: "this is post three body." }, getPosts)
-  .then(getPosts).then(deletePost(posts)).then(deletePost(posts)).then(deletePost(posts)).then(deletePost(posts)).catch(err => console.log("some thing went wrong."))
-
-
-
