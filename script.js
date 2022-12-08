dacef03b9c84a9e173d64c00f061e56e01cba67f
sanju5203 <>
2022-12-08 18:07:35 +0000

    person with ticket

--- script.js
+++ script.js
@@ -1,70 +1,15 @@
-const posts = [
-  { CreatedAt: new Date().getTime(), Title: "Post One", body: "this is post one body" },
-  { CreatedAt: new Date().getTime(), Title: "Post Two", body: "this is post two body" }
-];
+console.log('person1 : shows ticket')
+console.log('person2 : shows ticket')
 
-
-function getPosts() {
-  let output = ''
-  posts.forEach((post, index) => {
-    output += `<li>${post.Title} created ${Math.floor(msToTime(new Date().getTime() - post.CreatedAt))} seconds ago</li>`;
-  })
-  document.body.innerHTML = output
-  console.log(posts)
-}
- getPosts()
-//setInterval(getPosts, 1000);
-
-
-function deletePost(posts) {
+const promiseWifeBringingTicks = new Promise((resolve, reject) => {
   setTimeout(() => {
-    posts.pop()
-    console.log("deleted after 1 sec.")
-    getPosts()
-  }, 1000)
-};
+    resolve('ticket yes')
+  }, 3000)
+})
 
-function createPost(post) {
+promiseWifeBringingTicks.then((t) => {
+  console.log(`person 3 show ticket ${t}`)
+})
 
-  return new Promise((resolve, reject) => {
-    posts.push(post)
-    const error = false
-    if (!error) {
-      
-      console.log("new post created")
-      getPosts()
-      resolve()
-    } else {
-      reject("Error: something went wrong!")
-    }
-  });
-
-}
-
-const updateLastUserActivityTime = new Date();
-
-Promise.all([createPost({ CreatedAt: new Date().getTime(), Title: "Post four", body: "this is post three body." }, getPosts), updateLastUserActivityTime]).then(values => console.log(values)).then(getPosts).then(()=>{deletePost(posts)}).then(getPosts).catch(err => console.log("some thing went wrong."))
-
-
-
-
-function msToTime(s) {
-  var ms = s % 1000;
-  s = (s - ms) / 1000;
-  var secs = s % 60;
-  s = (s - secs) / 60;
-  var mins = s % 60;
-  var hrs = (s - mins) / 60;
-  if (hrs == 0 && mins == 0 && secs == 0) {
-    return ms;
-  }
-  else if (hrs == 0 && mins == 0) {
-    return secs + '.' + ms;
-  }
-  else if (hrs == 0) {
-    return mins + ':' + secs + '.' + ms;
-  } else {
-    return hrs + ':' + mins + ':' + secs + '.' + ms;
-  }
-
-}
+console.log('person4 : shows ticket')
+console.log('person5 : shows ticket')
