49fe396e94ff17842976032e378a4f5d58238442
sanju5203 <>
2022-12-08 18:11:53 +0000

    popcorn

--- script.js
+++ script.js
@@ -3,13 +3,20 @@ console.log('person2 : shows ticket')
 
 const promiseWifeBringingTicks = new Promise((resolve, reject) => {
   setTimeout(() => {
-    resolve('ticket yes')
+    resolve('ticket')
   }, 3000)
 })
 
-promiseWifeBringingTicks.then((t) => {
-  console.log(`person 3 show ticket ${t}`)
+const getPopcorn = promiseWifeBringingTicks.then((t) => {
+  console.log("husaband : we should go in .")
+  console.log(`wife : no i am hungry`)
+
+  return new Promise((resolve, reject) => resolve(` ${t} popcorn`))
 })
+
+getPopcorn.then((t) => console.log(t))
+
+
 
 console.log('person4 : shows ticket')
 console.log('person5 : shows ticket')
