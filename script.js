dd2746ad7e28ed4560c2ea8bec13bed47cd2c001
sanju5203 <>
2022-12-08 18:18:00 +0000

    button

--- script.js
+++ script.js
@@ -8,14 +8,23 @@ const promiseWifeBringingTicks = new Promise((resolve, reject) => {
 })
 
 const getPopcorn = promiseWifeBringingTicks.then((t) => {
+  console.log("Wife : I have the tickets.")
   console.log("husaband : we should go in .")
+
   console.log(`wife : no i am hungry`)
+
+  return new Promise((resolve, reject) => resolve(` ${t} butter`))
+})
+
+const getButter = getPopcorn.then((t) => {
+  console.log("husaband : I got some popcorn .")
+  console.log("husaband : we should go in .")
+  console.log(`wife : I need butter on my popcorn`)
 
   return new Promise((resolve, reject) => resolve(` ${t} popcorn`))
 })
 
-getPopcorn.then((t) => console.log(t))
-
+getButter.then((t) => console.log(t))
 
 
 console.log('person4 : shows ticket')
