cd5972ecbc1fe6cb3d99fe6f282ba207b84c96f6
sunil singh parihar <78852176+sunilsinghparihar5203@users.noreply.github.com>
2022-12-10 14:00:27 +0000

    converted to async await

--- script.js
+++ script.js
@@ -1,14 +1,33 @@
-const preMovie = async () => {
-  const promiseWifeBringingtickets = new Promise((resolve, reject) => {
-    setTimeout(() => reject('ticket'), 3000);
-  })
-  let ticket;
-  try {
-    ticket = await promiseWifeBringingtickets;
-  } catch (e) {
-    ticket = 'sad face';
-  }
-  return ticket
-};
+var posts = []
 
-preMovie().then((m) => console.log(`person 3 : show ${m}`));
+function createPost(post) {
+  return new Promise((resolve, reject) => {
+    posts.push(post)
+    console.log(post.Title + " created")
+    resolve(posts)
+    console.log(getpost())
+  });
+}
+
+function getpost() {
+      return posts
+}
+
+async function deletePost() {
+
+      await createPost({ CreatedAt: new Date().getTime(), Title: "Post one", body: "this is post one body." })
+        
+      await setTimeout(()=>{
+         if (posts.length > 0) {
+           posts.pop();
+           console.log("post removed")
+        }else{
+           console.log("posts not available")
+        }
+         console.log(getpost())
+      },5000)
+      
+  
+}
+
+ deletePost() 
